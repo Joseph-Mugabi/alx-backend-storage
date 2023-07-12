c23dfc92517237c4c79fc95e8dc5ae873b2774ed
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-12 20:23:06 +0300

    enumeration of countries

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,5 @@
 -- create a table users
 -- with attributes id, email, name, and country
-DROP TABLE users;
 CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
