d9ad4bf93f055bc43c211f1df95d184025e62c72
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-13 15:22:59 +0300

    function SafeDiv that divides (and returns) the first by the second number

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- creates a func SafeDiv that divides and returns
+-- the first by the second number
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNs FLOAT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END;
